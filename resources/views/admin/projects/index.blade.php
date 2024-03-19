89544b6f8414d2f5567bff457c81f293f86f90ce
FrancescoM95 <mon95fran@gmail.com>
2024-03-19 18:20:34 +0100

    bonus

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -14,7 +14,7 @@
             <th scope="col">Slug</th>
             <th scope="col">Descrizione</th>
             <th scope="col">Linguaggio</th>
-            <th scope="col">Creato il</th>
+            <th scope="col">Data creazione</th>
             <th scope="col">Ultima modifica</th>
             <th scope="col"></th>
           </tr>
@@ -30,9 +30,16 @@
                 <td>{{ $project->created_at }}</td>
                 <td>{{ $project->updated_at }}</td>
                 <td>
+                  <div class="d-flex gap-2">
                     <a href="{{ route('admin.projects.show', $project)}}" class="btn btn-sm btn-primary">
                       <i class="far fa-eye"></i>
                     </a>
+                    <form action="{{ route('admin.projects.destroy', $project->id) }}" method="POST" id="delete-form">
+                      @csrf
+                      @method('DELETE')
+                      <button class="btn btn-sm btn-danger"><i class="far fa-trash-can"></i></button>
+                    </form>
+                  </div>
                 </td>
               </tr>
             @empty
@@ -46,4 +53,18 @@
           
         </tbody>
       </table>
+@endsection
+
+@section('scripts')
+    <script>
+        const deleteForm = document.getElementById('delete-form');
+
+        deleteForm.addEventListener('submit', e => {
+            e.preventDefault();
+
+            const confirmation = confirm('Sei sicuro di voler eliminare questo progetto?');
+
+            if(confirmation) deleteForm.submit();
+        });
+    </script>
 @endsection
